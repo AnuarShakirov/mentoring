567a6485761e5bbb25127c01fd7efc274296a449
anuar.shakirov <anuar.shakirov@aramcoinnovations.com>
2024-11-25 19:09:50 +0300

    Hydra: first config

---
+++ src/main.py
@@ -0,0 +1,15 @@
+"""Module to show examples about hydra."""
+
+import pandas as pd 
+import hydra
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="hydra")
+
+
+@hydra.main(config_path=".", config_name="config_main", version_base="1.1")
+def main_func(cfg) -> None:
+    """Example main function."""
+    print(cfg)
+
+if __name__ == "__main__":
+    main_func()
